05e1bb804ed7e0205622c2d0ebefcbf91e464a4e
Abhijeet8962 <111488099+Abhijeet8962@users.noreply.github.com>
2025-06-09 23:18:16 +0530

    update user model

--- models/user.model.js
+++ models/user.model.js
@@ -4,8 +4,11 @@ const userSchema = new mongoose.Schema({
   name: { type: String, required: true },
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
-  role: { type: String, default: 'user' },
+  role: { type: String, required: true, default: 'user' },
+  status: { type: Boolean, required: true, default: true },
+  isDeleted: { type: Boolean, required: true, default: false },
   createdAt: { type: Date, default: Date.now },
+  updatedAt: { type: Date, default: Date.now },
 });
 
 module.exports = mongoose.models.User || mongoose.model('User', userSchema);
