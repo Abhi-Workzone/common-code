63e2e21d0c2bc51b229d997ea41afff0439999a6
Abhijeet8962 <111488099+Abhijeet8962@users.noreply.github.com>
2025-06-23 21:21:25 +0530

    update permission model 1.0.9

--- models/permission.model.js
+++ models/permission.model.js
@@ -2,19 +2,11 @@
 const mongoose = require('mongoose');
 const constant = require('../utils/constant');
 
-const actionSchema = new mongoose.Schema(
-  {
-    key: { type: String, required: true },     // e.g., "create", "update"
-    label: { type: String, required: true }    // e.g., "Create User"
-  },
-  { _id: false } // Don't auto-create _id for each action
-);
-
 const permissionSchema = new mongoose.Schema(
   {
     module: { type: String, required: true, unique: true },     // e.g., "user", "event"
     displayName: { type: String, required: true },              // e.g., "User Management"
-    actions: { type: [actionSchema], default: [] }              // actions with key+label
+    actions: { type: [String], default: [] }              // actions with key+label
   },
   { timestamps: true }
 );
