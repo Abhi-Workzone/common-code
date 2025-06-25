625fdbae9a7db27262fcc1a60d1e98d969ea3a5e
Abhijeet8962 <111488099+Abhijeet8962@users.noreply.github.com>
2025-06-25 21:31:26 +0530

    update user model 1.0.12

--- models/user.model.js
+++ models/user.model.js
@@ -6,7 +6,7 @@ const userSchema = new mongoose.Schema(
     name: { type: String, required: true },
     email: { type: String, required: true, unique: true },
     password: { type: String, required: true },
-    role: [{ type: mongoose.Schema.Types.ObjectId, required: true, ref: 'Role' }],
+    roles: [{ type: mongoose.Schema.Types.ObjectId, required: true, ref: 'Role' }],
     status: { type: Boolean, required: true, default: true },
     isDeleted: { type: Boolean, required: true, default: false }
   },
